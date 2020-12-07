4c284d82f3bf49cd18ea74d5b054e2114143ab52
CloudWalkerW <willis.wu@funpodium.net>
2020-12-07 23:30:52 +0800

    Fix few bugs, stuck at first cycle

--- codes/Forwarding_Unit.v
+++ codes/Forwarding_Unit.v
@@ -16,12 +16,12 @@ output [1:0] ForwardA_o , ForwardB_o;
 reg [1:0] ForwardA_o , ForwardB_o;
 
 always @(posedge clk_i or EX_rs1_i or EX_rs2_i or MEM_Rd_i or MEM_RegWrite_i or WB_RegWrite_i or WB_Rd_i) begin
-    if (MEM_RegWrite_i && MEM_Rd_i && MEM_Rd_i == EX_Rs1_i) ForwardA_o <= 2'b10; //EX hazard
-    else if (WB_RegWrite_i && WB_Rd_i && WB_Rd_i == EX_Rs1_i) ForwardA_o <= 2'b01; //MEM hazard
+    if (MEM_RegWrite_i && MEM_Rd_i && MEM_Rd_i == EX_rs1_i) ForwardA_o <= 2'b10; //EX hazard
+    else if (WB_RegWrite_i && WB_Rd_i && WB_Rd_i == EX_rs1_i) ForwardA_o <= 2'b01; //MEM hazard
     else ForwardA_o <= 2'b00;
 
-    if (MEM_RegWrite_i && MEM_Rd_i && MEM_Rd_i == EX_Rs2_i) ForwardB_o <= 2'b10; //EX hazard
-    else if (WB_RegWrite_i && WB_Rd_i && WB_Rd_i == EX_Rs2_i) ForwardB_o <= 2'b01; // MEM hazard
+    if (MEM_RegWrite_i && MEM_Rd_i && MEM_Rd_i == EX_rs2_i) ForwardB_o <= 2'b10; //EX hazard
+    else if (WB_RegWrite_i && WB_Rd_i && WB_Rd_i == EX_rs2_i) ForwardB_o <= 2'b01; // MEM hazard
     else ForwardB_o <= 2'b00;
 
 end
--- codes/MUX_Forwarding.v
+++ codes/MUX_Forwarding.v
@@ -6,8 +6,8 @@ module MUX_Forwarding(
     data_o 
 );
 `define ID_EX 2'b00
-`define EX_MEM 2'b01
-`define MEM_WB 2'b10
+`define EX_MEM 2'b10
+`define MEM_WB 2'b01
 
 input [31:0] data00_i , data01_i , data10_i;
 input [1:0] Forward_i;
@@ -17,8 +17,8 @@ reg [31:0] data_o;
 always @(data00_i or data01_i or data10_i or Forward_i) begin
     case (Forward_i)
         `ID_EX:  data_o = data00_i;
-        `EX_MEM:  data_o = data01_i;
-        `MEM_WB:  data_o = data10_i;
+        `EX_MEM:  data_o = data10_i;
+        `MEM_WB:  data_o = data01_i;
     endcase
 end
 
